7c858a8b522c8eefd1974ee0677334592aafe2d4
Nicolas Vanalderweireldt <n.vanalder@gmail.com>
2017-01-30 21:05:13 +0100

    revert modifs

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: {
+    javascript: "./js/app.js",
+    html: "./index.html"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "./js/app.js"
+  }
+};
